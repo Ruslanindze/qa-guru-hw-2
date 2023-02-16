ac47bc0bda65ffd2c55a6d619013c144157cc644
Ruslanindze <42635477+Ruslanindze@users.noreply.github.com>
2023-02-16 22:27:37 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Testing sum");
         System.out.println("Case_001: 5 + 3 == 8 => " + (sum(5, 3) == 8));
         System.out.println("Case_002: 5 + 4 == 9 => " + (sum(5, 4) == 9));
-        System.out.println("Case_003: 5 + 5 == 10 => " + (sum(5, 5) == 11));
+        System.out.println("Case_003: 5 + 5 == 10 => " + (sum(5, 5) == 10));
     }
 
     static int sum(int x, int y) {
