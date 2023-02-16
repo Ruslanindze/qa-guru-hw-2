297e17d96f5599efdd7b6d26f5a6cbd7f57676c5
Ruslanindze <42635477+Ruslanindze@users.noreply.github.com>
2023-02-16 22:42:28 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String... args) {
 
         System.out.println("Testing sum");
         System.out.println("Case_001: 5 + 3 == 8 => " + (sum(5, 3) == 8));
