79ef48cc85165c608e985356fe42290e3fad92aa
Ruslanindze <42635477+Ruslanindze@users.noreply.github.com>
2023-02-16 22:24:10 +0300

    Add cases

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Testing sum");
+        System.out.println("Case_001: 5 + 3 == 8 => " + (sum(5, 3) == 8));
+        System.out.println("Case_002: 5 + 4 == 9 => " + (sum(5, 4) == 9));
+        System.out.println("Case_003: 5 + 5 == 10 => " + (sum(5, 5) == 11));
+    }
+
+    static int sum(int x, int y) {
+        return  x + y;
     }
 }
